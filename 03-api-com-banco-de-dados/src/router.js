44e9c25940a164d9477944fd586cdf7caf818d97
Lukas <lukasnatanael889@gmail.com>
2025-11-05 07:24:44 -0300

    feat(exe-03): unificado rotas de products e costumers

--- 03-api-com-banco-de-dados/src/router.js
+++ 03-api-com-banco-de-dados/src/router.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const productsController = require('./controllers/products-controller')
+const costumersController = require('./controllers/costumers-controller')
 
 const router = Router()
 
@@ -8,5 +9,11 @@ router.get('/products/:id',    productsController.show )
 router.post('/products',       productsController.create )
 router.put('/products/:id',    productsController.update )
 router.delete('/products/:id', productsController.delete )
+
+router.get('/costumers',        costumersController.index )
+router.get('/costumers/:id',    costumersController.show )
+router.post('/costumers',       costumersController.create )
+router.put('/costumers/:id',    costumersController.update )
+router.delete('/costumers/:id', costumersController.delete )
 
 module.exports = router
