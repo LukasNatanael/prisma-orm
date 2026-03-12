64d9852b6ea1a898e7a102c19ac441b15fcf8e4c
Lukas <lukasnatanael889@gmail.com>
2026-03-12 07:29:28 -0300

    criado rotas das tags

---
+++ 04-conhecendo-o-prisma/src/routes/tags.js
@@ -0,0 +1,45 @@
+const { Router } = require('express')
+const prisma = require('../database')
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const tags = await prisma.tag.findMany()
+  res.json(tags)
+})
+
+router.post('/', async (req, res) => {
+  const { name } = req.body
+  const tag = await prisma.tag.create({
+    data: { name },
+  })
+  res.status(201).json(tag)
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const tags = await prisma.tag.findUnique({
+    where: { id: Number(id) },
+    include: { posts: true }
+    
+  })
+  res.json(tags)
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const { name } = req.body
+  const tag = await prisma.tag.update({
+    data: { name },
+    where: { id: Number(id) }
+  })
+  res.json(tag)
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  const tag = await prisma.tag.delete({ where: { id: Number(id) } })
+  res.json(tag)
+}) 
+
+module.exports = router
